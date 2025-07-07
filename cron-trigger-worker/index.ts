f289cc2963afa4a2e734e3c58959d41f6a52293b
michael <michaelcosj@proton.me>
2025-07-07 01:21:34 +0100

    added logging to worker code

--- cron-trigger-worker/index.ts
+++ cron-trigger-worker/index.ts
@@ -9,11 +9,15 @@ export default {
 		env: { CRON_SECRET: string; SVELTEKIT_URL: string },
 		_ctx: ExecutionContext,
 	) {
-		return await fetch(env.SVELTEKIT_URL + "/api/run", {
+		const endpoint = `${env.SVELTEKIT_URL}/api/run`;
+		console.log(`sending request to ${endpoint}`);
+
+		const response = await fetch(endpoint, {
 			method: "POST",
 			headers: {
 				Authorization: `Bearer ${env.CRON_SECRET}`,
 			},
 		});
+		console.log("api responded with: ", response.status, response.statusText);
 	},
 };
