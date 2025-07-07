4564c1f1705f29c8b9930cc7a6becfdfa55850be
michael <michaelcosj@proton.me>
2025-07-07 01:51:49 +0100

    update to use cloudflare service bindings

--- cron-trigger-worker/index.ts
+++ cron-trigger-worker/index.ts
@@ -1,23 +1,31 @@
 import type {
 	ExecutionContext,
 	ScheduledController,
+	Service,
 } from "@cloudflare/workers-types";
+
+interface Env {
+	CRON_SECRET: string;
+	WORKFLOW_URL: string;
+	WORKFLOW: Service;
+}
 
 export default {
 	async scheduled(
 		_controller: ScheduledController,
-		env: { CRON_SECRET: string; SVELTEKIT_URL: string },
+		env: Env,
 		_ctx: ExecutionContext,
 	) {
-		const endpoint = `${env.SVELTEKIT_URL}/api/run`;
+		const endpoint = `${env.WORKFLOW_URL}/api/run`;
 		console.log(`sending request to ${endpoint}`);
 
-		const response = await fetch(endpoint, {
+		const response = await env.WORKFLOW.fetch(endpoint, {
 			method: "POST",
 			headers: {
 				Authorization: `Bearer ${env.CRON_SECRET}`,
 			},
 		});
+
 		console.log("api responded with: ", response.status, response.statusText);
 	},
 };
